0633caa57299658445e51cfc5d4a9453451709d6
Ammen1 <amenguda@gmail.com>
2024-02-02 17:25:27 +0300

    tailwind confing file add

--- src/App.jsx
+++ src/App.jsx
@@ -2,5 +2,5 @@
 import React from "react";
 
 export default function App() {
-  return <div>App</div>;
+  return <div> Well come to this Blog</div>;
 }
---
+++ src/components/DashboardComp.1.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function DashboardComp() {
+  return <div>DashboardComp</div>;
+}
